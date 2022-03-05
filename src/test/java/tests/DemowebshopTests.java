763515a3a5fe7f0a3743730788fb4327a994c644
Elizaveta Azovtseva <azovcevae@gmail.com>
2022-03-05 17:17:15 +0700

    practice from lesson "REST API tests" in DemoWebShop

---
+++ src/test/java/tests/DemowebshopTests.java
@@ -0,0 +1,62 @@
+package tests;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class DemowebshopTests {
+
+    @Test
+    void addToCartAsNewUserTest() {
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body("product_attribute_72_5_18=53" +
+                        "&product_attribute_72_6_19=54" +
+                        "&product_attribute_72_3_20=57" +
+                        "&addtocart_72.EnteredQuantity=1")
+                .when()
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your " +
+                        "<a href=\"/cart\">shopping cart</a>"))
+                .body("updatetopcartsectionhtml", is("(1)"));
+    }
+
+    @Test
+    void addToCartWithCookieTest() {
+        Integer cartSize = 0;
+
+        ValidatableResponse response =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .cookie("Nop.customer=88f590c6-59e9-4a55-b243-7395b35f0ce2;")
+                        .body("product_attribute_72_5_18=53" +
+                                "&product_attribute_72_6_19=54" +
+                                "&product_attribute_72_3_20=57" +
+                                "&addtocart_72.EnteredQuantity=1")
+                        .when()
+                        .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("success", is(true))
+                        .body("message", is("The product has been added to your " +
+                                "<a href=\"/cart\">shopping cart</a>"));
+
+//assertThat(response.extract().path("updatetopcartsectionhtml").toString())
+// .body("updatetopcartsectionhtml", is("(43)"));*/
+
+    }
+
+    /*HttpResponse<String> response = Unirest.post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
+  .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+  .header("Cookie", "ARRAffinity=a1e87db3fa424e3b31370c78def315779c40ca259e77568bef2bb9544f63134e; Nop.customer=88f590c6-59e9-4a55-b243-7395b35f0ce2; Nop.customer=88f590c6-59e9-4a55-b243-7395b35f0ce2")
+  .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54&product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
+  .asString();
+     */
+}
