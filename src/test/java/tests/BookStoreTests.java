025e6b1c881f915e8cea1f34711c5bef96bf58d4
Elizaveta Azovtseva <azovcevae@gmail.com>
2022-03-10 12:20:52 +0700

    add Allure report with CustomTemplates() and check validate JsonSchema of response

--- src/test/java/tests/BookStoreTests.java
+++ src/test/java/tests/BookStoreTests.java
@@ -2,24 +2,25 @@ package tests;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 public class BookStoreTests {
 
     @BeforeAll
-    static void setup(){
+    static void setup() {
         RestAssured.baseURI = "https://demoqa.com";
     }
 
     @Test
-    void getBookTest(){
+    void getBookTest() {
         //curl -X GET "https://demoqa.com/BookStore/v1/Books" -H "accept: application/json"
         get("/BookStore/v1/Books")
                 .then()
@@ -27,7 +28,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void getBooksWithAllLogsTest(){
+    void getBooksWithAllLogsTest() {
         given()
                 .log().uri()
                 .log().body()
@@ -40,7 +41,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void generateTokenTest(){
+    void generateTokenTest() {
         String data = "{ \"userName\": \"alex\", " +
                 "\"password\": \"asdsad#frew_DFS2\" }";
         given()
@@ -59,7 +60,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void generateTokenTestWithAllureListener(){
+    void generateTokenTestWithAllureListener() {
         String data = "{ \"userName\": \"alex\", " +
                 "\"password\": \"asdsad#frew_DFS2\" }";
         given()
@@ -73,6 +74,28 @@ public class BookStoreTests {
                 .then()
                 .log().status()
                 .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", greaterThan(10));
+    }
+
+    @Test
+    void generateTokenTestWithCustomAllureListener() {
+        String data = "{ \"userName\": \"alex\", " +
+                "\"password\": \"asdsad#frew_DFS2\" }";
+        given()
+                .filter(withCustomTemplates())
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/generateToken_response_shema.json"))
                 .body("status", is("Success"))
                 .body("result", is("User authorized successfully."))
                 .body("token.size()", greaterThan(10));
